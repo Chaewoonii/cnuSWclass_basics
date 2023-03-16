341a455d39f02d1b22c7b66795ee4e09775582af
ChacunKim <cocodata12@gmail.com>
2023-03-16 17:40:44 +0900

    Spring Basic study 230316

--- kdt-spring-order/src/main/java/org/prgrms/kdt/SimpleKdtApplication.java
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/SimpleKdtApplication.java
@@ -1,14 +1,29 @@
 package org.prgrms.kdt;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "org.prgrms.kdt.customer")
-public class SimpleKdtApplication {
+public class SimpleKdtApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleKdtApplication.class, args); //Spring boot은 embeded Tomcat을 통해 별도의 설정 없이도 서버를 띄운다.
     }
+
+
 }
---
+++ kdt-spring-order/src/main/java/org/prgrms/kdt/config/MvcConfig.java
@@ -0,0 +1,38 @@
+package org.prgrms.kdt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    //WebMvcConfigurer 관련 설정: @Configuration 이 붙은 클래스 혹은 @SpringBootApplication 에서 처리할 수 있다.
+    //@Configuration에 설정할 경우 @ComponentScan의 basePackage에 설정 클래스가 포함된 패키지를 추가해야 한다.
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("*");
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //xml
+        var messageConverter = new MarshallingHttpMessageConverter();
+        var xStreamMarshaller = new XStreamMarshaller();
+        messageConverter.setMarshaller(xStreamMarshaller);
+        messageConverter.setUnmarshaller(xStreamMarshaller);
+
+        //index 0: xml 형식의 message converter 를 제일 앞에 둠
+        converters.add(0, messageConverter);
+
+        //javaTimeModule은 Spring에서 알아서 처리해줌.
+        /*var javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+        var modules = Jackson2ObjectMapperBuilder.json().modules(javaTimeModule);
+        converters.add(1, new MappingJackson2HttpMessageConverter(modules.build()));*/
+    }
+}
